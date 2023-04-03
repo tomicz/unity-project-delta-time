6e30c1e08e690b6774560d72a1ebf69555343853
Darko Tomic <tomiczdarko@gmail.com>
2023-04-03 22:52:57 +0200

    Added basic scene and UI setup

--- Scripts/DeltaTimeController.cs
+++ Scripts/DeltaTimeController.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using TMPro;
 
 public class DeltaTimeController : MonoBehaviour
 {
-        // Start is called before the first frame update
-    void Start()
+    [Header("Basic properties")]
+    [SerializeField] private ushort _targetFps = 60;
+    [SerializeField] private float _targetSpeed = 1;
+
+    [Header("Dependencies")]
+    [SerializeField] private Transform _objectA;
+    [SerializeField] private Transform _objectB;
+
+    [Header("UI")]
+    [SerializeField] private TMP_Text _targetFPSText;
+    [SerializeField] private TMP_Text _currentFPSText;
+
+
+    private void Awake()
     {
-        
+        Application.targetFrameRate = _targetFps;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        _objectA.transform.position += new Vector3(0f, _targetSpeed, 0f);
+        _objectB.transform.position += new Vector3(0f, _targetSpeed, 0f) * Time.deltaTime;
+
+        UpdateUI();
+    }
+
+    private void UpdateUI()
+    {
+        float fps = Time.frameCount / Time.time;
+        _targetFPSText.text = $"Target fps: {_targetFps}";
+        _currentFPSText.text = $"Current fps: {fps.ToString("F1")}";
     }
 }
