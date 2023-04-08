ccdfefbdfb877a03df9b8602bb83ee488f8428d1
Darko Tomic <tomiczdarko@gmail.com>
2023-04-08 16:17:54 +0200

    Added deltaTime and a vehicle for better visualisation

--- Scripts/FrameCounterController.cs
+++ Scripts/FrameCounterController.cs
@@ -1,47 +1,88 @@
-using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class FrameCounterController : MonoBehaviour
 {
+    [Header("Properties")]
+    [SerializeField] private int _frameCount = 60;
+    [SerializeField] private float _moveSpeed;
+
     [Header("Dependencies")]
     [SerializeField] private Image _frame;
     [SerializeField] private Image _lastFrame;
     [SerializeField] private Image _currentFrame;
     [SerializeField] private Image _deltaTime;
+    [SerializeField] private Image _car;
+    [SerializeField] private Toggle _isDeltaTimeEnabledToggle;
 
     [Header("Transforms")]
     [SerializeField] private Transform _frameContainer;
     [SerializeField] private Transform _startPosition;
     [SerializeField] private Transform _endPosition;
 
-    [Header("Properties")]
-    [SerializeField] private int _frameCount = 60;
-
     private Transform[] _framesArray;
     private int _frameIndex = 0;
+    private bool _isEventStarted = false;
+    private bool _isDeltaTimeEnabled = false;
 
     private void Awake()
     {
         CreateFrames();
+        InitiliseMovingEntity();
     }
 
     public void StartEvent()
     {
-        //InitiliseFrames();
-        //StartCoroutine(StartFrameCounting());
+        _isEventStarted = true;
+        ToggleDeltaTime();
     }
 
     public void StopEvent()
     {
-        //StopAllCoroutines();
-        //InitiliseFrames();
+        _isEventStarted = false;
+        ResetPositions();
     }
+
+    public void EnableDeltaTime() => _isDeltaTimeEnabled = true;
+
+    public void DisableDeltaTime() => _isDeltaTimeEnabled = false;
 
     private void Update()
     {
+        if (!_isEventStarted) return;
+
         TrackLastAndCurrentFrame();
+        PushMovingEntityForward();
+    }
+
+    private void InitiliseMovingEntity()
+    {
+        _car.transform.position = new Vector2(_framesArray[0].position.x, _framesArray[0].position.y + 60f);
+    }
+
+    private void PushMovingEntityForward()
+    {
+        if (_isDeltaTimeEnabled)
+        {
+            _car.transform.position += Vector3.right * (_moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            _car.transform.position += Vector3.right * _moveSpeed;
+        }
+    }
+
+    private void ToggleDeltaTime()
+    {
+        if (_isDeltaTimeEnabledToggle.isOn)
+        {
+            _isDeltaTimeEnabled = true;
+        }
+        else
+        {
+            _isDeltaTimeEnabled = false;
+        }
     }
 
     private void CreateFrames()
@@ -77,7 +118,7 @@ public class FrameCounterController : MonoBehaviour
         _lastFrame.transform.position = _framesArray[_frameIndex].position;
         _currentFrame.transform.position = _framesArray[_frameIndex + 1].position;
 
-        UpdateDeltaTime((_currentFrame.transform.position.x - _lastFrame.transform.position.x));
+        UpdateDeltaTime(_currentFrame.transform.position.x - _lastFrame.transform.position.x);
     }
 
     private void UpdateDeltaTime(float frameOffset)
@@ -87,5 +128,15 @@ public class FrameCounterController : MonoBehaviour
         _deltaTime.GetComponent<RectTransform>().sizeDelta = size;
         _deltaTime.transform.position = new Vector2((_currentFrame.transform.position.x + _lastFrame.transform.position.x) / 2, _framesArray[0].transform.position.y);
         _deltaTime.GetComponentInChildren<TMP_Text>().text = Time.deltaTime.ToString("F4");
+    }
+
+    private void ResetPositions()
+    {
+        _lastFrame.transform.position = _framesArray[0].position;
+        _currentFrame.transform.position = _framesArray[1].position;
+
+        UpdateDeltaTime(_currentFrame.transform.position.x - _lastFrame.transform.position.x);
+
+        _car.transform.position = new Vector2(_framesArray[0].position.x, _framesArray[0].position.y + 60f);
     }
 }
