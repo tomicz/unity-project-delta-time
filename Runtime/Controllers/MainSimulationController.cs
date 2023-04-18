479f21635b9822763cec2867d512688dfa6c0053
Darko Tomic <tomiczdarko@gmail.com>
2023-04-18 18:14:17 +0200

    Input fields added

--- Runtime/Controllers/MainSimulationController.cs
+++ Runtime/Controllers/MainSimulationController.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MainSimulationController : MonoBehaviour
@@ -6,9 +7,11 @@ public class MainSimulationController : MonoBehaviour
     [Header("Dependencies")]
     [SerializeField] private UIViewSimulationContainer _uiViewSimulationContainerPrefab = null;
     [SerializeField] private Transform _simulationsContainer = null;
+    [SerializeField] private TMP_Text _pauseButtonText;
 
     private List<UIViewSimulationContainer> _containersList = new List<UIViewSimulationContainer>();
     private int _count = 0;
+    private bool _areEventsPaused = false;
 
     private void Start()
     {
@@ -38,5 +41,44 @@ public class MainSimulationController : MonoBehaviour
     {
         _containersList[id].gameObject.SetActive(false);
         _count--;
+    }
+
+    public void Run()
+    {
+        foreach (var simulator in _containersList)
+        {
+            simulator.Run();
+        }
+
+        _pauseButtonText.text = "Pause";
+    }
+
+    public void Pause()
+    {
+        if (_areEventsPaused)
+        {
+            _areEventsPaused = false;
+            _pauseButtonText.text = "Resume";
+        }
+        else
+        {
+            _areEventsPaused = true;
+            _pauseButtonText.text = "Pause";
+        }
+
+        foreach (var simulator in _containersList)
+        {
+            simulator.Pause(_areEventsPaused);
+        }
+    }
+
+    public void Stop()
+    {
+        foreach (var simulator in _containersList)
+        {
+            simulator.Stop();
+        }
+
+        _pauseButtonText.text = "Pause";
     }
 }
--- Runtime/UIViews/UIViewSimulationContainer.cs
+++ Runtime/UIViews/UIViewSimulationContainer.cs
@@ -11,22 +11,42 @@ public class UIViewSimulationContainer : MonoBehaviour
     [SerializeField] private Toggle _enableFixedDeltaTimeToggle = null;
     [SerializeField] private TMP_InputField _vehicleSpeedInputField = null;
     [SerializeField] private Toggle _loopType = null;
+    [SerializeField] private TMP_Text _loopTypeText = null;
     [SerializeField] private Image _vehicle;
     [SerializeField] private Transform _endPositionTransform;
     [SerializeField] private RectTransform _distanceImage;
     [SerializeField] private RectTransform _race;
 
+    [Header("Colors")]
+    [SerializeField] private Color _defaultColor;
+    [SerializeField] private Color _updateColor;
+    [SerializeField] private Color _fixedUpdateColor;
+    [SerializeField] private Color _textUpdateColor;
+    [SerializeField] private Color _textFixedUpdateColor;
+
+    [SerializeField] private Image[] _headerImageButtons;
+
     private MainSimulationController _mainSimulationController;
     private int _id;
+
+    private bool _hasSimulatorStarted = false;
+    private bool _isSimulatorPaused = false;
+    private bool _isUpdateRunning = true;
 
     private Vector3 _startPosition;
     private Vector3 _endPosition;
     private float _distance = 0;
     private float _vehicleSpeed = 0;
 
+    private void Awake()
+    {
+        _loopType.onValueChanged.AddListener(delegate { OnLoopSwitchToggle(_loopType); });
+    }
+
     private void OnEnable()
     {
         LayoutRebuilder.MarkLayoutForRebuild(GetComponent<RectTransform>());
+        ChangeHeaderColors(_updateColor, _textUpdateColor);
     }
 
     private void Start()
@@ -40,14 +60,31 @@ public class UIViewSimulationContainer : MonoBehaviour
     {
         yield return new WaitForEndOfFrame();
 
-        _startPosition = new Vector3(_race.anchoredPosition.x - _race.rect.width / 2, 0, 0);
+        _startPosition = new Vector3(_race.anchoredPosition.x - _race.rect.width / 2 + (_vehicle.GetComponent<RectTransform>().rect.width / 2), 0, 0);
         _endPosition = new Vector3(_endPositionTransform.position.x - (_vehicle.GetComponent<RectTransform>().rect.width / 2), 0, 0);
         _vehicle.transform.localPosition = _startPosition;
     }
 
     private void Update()
     {
-        MoveVehicleRight();
+        if (!_hasSimulatorStarted) return;
+        if (_isSimulatorPaused) return;
+
+        if (_isUpdateRunning)
+        {
+            MoveVehicleRight();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_hasSimulatorStarted) return;
+        if (_isSimulatorPaused) return;
+
+        if (!_isUpdateRunning)
+        {
+            MoveVehicleRight();
+        }
     }
 
     public void SetSimulationName(string name) => _nameText.text = name;
@@ -59,13 +96,58 @@ public class UIViewSimulationContainer : MonoBehaviour
     public void Remove()
     {
         _mainSimulationController.Remove(_id);
+
+        Reset();
+    }
+
+    public void Run()
+    {
+        _hasSimulatorStarted = true;
+        _vehicle.transform.localPosition = _startPosition;
+        _isSimulatorPaused = false;
+    }
+
+    public void Pause(bool isPaused)
+    {
+        _isSimulatorPaused = isPaused;
+    }
+
+    public void Stop()
+    {
+        Reset();
+    }
+
+    public void SetSpeed()
+    {
+        _vehicleSpeed = float.Parse(_vehicleSpeedInputField.text);
+    }
+
+    private void OnLoopSwitchToggle(Toggle toggle)
+    {
+        if (toggle.isOn)
+        {
+            _loopTypeText.text = $"Loop: FixedUpdate";
+            ChangeHeaderColors(_fixedUpdateColor, _textFixedUpdateColor);
+            _isUpdateRunning = false;
+        }
+        else
+        {
+            _isUpdateRunning = true;
+            _loopTypeText.text = $"Loop: Update";
+            ChangeHeaderColors(_updateColor, _textUpdateColor);
+        }
     }
 
     private void MoveVehicleRight()
     {
-        if (HasVehicleReachedFinishLine()) return;
-
-        _vehicle.transform.localPosition += Vector3.right * _vehicleSpeed;
+        if (HasVehicleReachedFinishLine())
+        {
+            _hasSimulatorStarted = false;
+        }
+        else
+        {
+            _vehicle.transform.localPosition += Vector3.right * _vehicleSpeed;
+        }
     }
 
     private bool HasVehicleReachedFinishLine()
@@ -76,5 +158,21 @@ public class UIViewSimulationContainer : MonoBehaviour
         }
 
         return false;
+    }
+
+    private void ChangeHeaderColors(Color buttonColor, Color textColor)
+    {
+        foreach (var image in _headerImageButtons)
+        {
+            image.color = buttonColor;
+            image.transform.GetChild(0).GetComponent<TMP_Text>().color = textColor;
+        }
+    }
+
+    private void Reset()
+    {
+        _vehicle.transform.localPosition = _startPosition;
+        _isSimulatorPaused = false;
+        _hasSimulatorStarted = false;
     }
 }
